759ec2f4f3ababf1c39d9d84264943b7d30f5c57
Dom <suriamTest@gmail.com>
2026-08-29 21:49:09 +0700

    ix production

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,9 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL ?? "http://localhost:3000",
-    credentials: true,
-  });
+  origin: [
+    "http://localhost:3000",
+    "https://ai-adaptive-learning-web.onrender.com",
+  ],
+  credentials: true,
+});
 
   app.setGlobalPrefix("api");
 
